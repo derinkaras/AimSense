2b32b46caf335669034e851170997dae9f56ef94
Derin <derinkaras@gmail.com>
2025-12-02 16:23:47 -0700

    Initial commit

---
+++ src/main/java/com/derinkaras/aimsense/AimSenseApplication.java
@@ -0,0 +1,13 @@
+package com.derinkaras.aimsense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AimSenseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/derinkaras/aimsense/AimSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derinkaras.aimsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimSenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
