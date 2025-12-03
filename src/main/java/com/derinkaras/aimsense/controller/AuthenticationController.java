7e4a21547da8e93618fffcdaa5e04205f30a6780
Derin <derinkaras@gmail.com>
2025-12-02 20:03:40 -0700

    Almost done

---
+++ src/main/java/com/derinkaras/aimsense/controller/AuthenticationController.java
@@ -0,0 +1,64 @@
+package com.derinkaras.aimsense.controller;
+
+import com.derinkaras.aimsense.dto.LoginUserDto;
+import com.derinkaras.aimsense.dto.RegisterUserDto;
+import com.derinkaras.aimsense.dto.VerifyUserDto;
+import com.derinkaras.aimsense.model.User;
+import com.derinkaras.aimsense.responses.LoginResponse;
+import com.derinkaras.aimsense.service.AuthenticationService;
+import com.derinkaras.aimsense.service.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(
+            JwtService jwtService,
+            AuthenticationService authenticationService
+    ) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponse loginResponse =
+                new LoginResponse(jwtToken, jwtService.getExpirationTime());
+
+        return ResponseEntity.ok(loginResponse);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verifyUser(@RequestBody VerifyUserDto verifyUserDto) {
+        try {
+            authenticationService.verifyUser(verifyUserDto);
+            return ResponseEntity.ok("Account verified successfully");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/resend")
+    public ResponseEntity<?> resendVerificationCode(@RequestParam String email) {
+        try {
+            authenticationService.resendVerificationCode(email);
+            return ResponseEntity.ok("Verification code sent");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
